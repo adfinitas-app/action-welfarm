7040ea955779b7f7a7255f93f786eb0e61b10ca9
Adfinitas Tech2 <adfinitech2@dhcp114.adfinitas.fr>
2017-12-07 16:47:14 +0100

    add campagne

---
+++ campagne/assets/js/main.js
@@ -0,0 +1,159 @@
+var slide1 = '<div id="left-slide"><img id="logo-header" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/logo-header.png"><a><img id="droite" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/droite.png"></a><a><img id="gauche" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/gauche.png"></a><img id="slide" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/first-slide.jpg"><div id="nav"><a><img style="padding-left:3px;" id="slide_1" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-on.png"></a><img style="padding-left:3px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-link.png"><a><img style="padding-left:3px;" id="slide_2" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-off.png"></a><img style="padding-left:3px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-link.png"><a><img style="padding-left:3px;" id="slide_3" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-off.png"></a></div></div><div id="right-slide"><img id="pointe" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/Rectangle+5.png"><div id="right-container" class="row"><div class="columns small-8 small-offset-1 end"><p id="poules">CES POULES NE VERRONT LA LUMIÈRE DU JOUR<br /><b id="abattoir">QUE POUR PARTIR À L\'ABATTOIR</b></p><p id="agis">J\'agis pour l\'accès des animaux au plein air</p><a href="#part-don"><img id="slide1-don" style="padding-top: 30px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/btn+don.png"></a></div></div><img id="dome" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/dome_blanc.png"></div>';
+var slide2 = '<div id="left-slide"><img id="logo-header" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/logo-header.png"><a><img id="droite" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/droite.png"></a><a><img id="gauche" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/gauche.png"></a><img id="slide" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/slide+2.jpg"><div id="nav"><a><img style="padding-left:3px;" id="slide_1" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-off.png"></a><img style="padding-left:3px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-link.png"><a><img style="padding-left:3px;" id="slide_2" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-on.png"></a><img style="padding-left:3px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-link.png"><a><img style="padding-left:3px;" id="slide_3" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-off.png"></a></div></div><div id="right-slide-2"><img id="pointe" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/Rectangle+5+noir.png"><div id="right-container" class="row"><div class="columns medium-11 medium-offset-1 large-8 large-offset-1 end"><p id="poules">CES JUMENTS ONT ÉTÉ<br /><b id="abattoir">AVORTÉES SANS ANESTHÉSIE POUR PRODUIRE UNE HORMONE</b></p><p id="agis">J\'agis contre les maltraitances et mutilations</p><a href="#part-don"><img id="slide2-don" style="padding-top: 30px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/btn+don+orange.png"></a></div></div><img id="dome" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/dome_blanc.png"></div>';
+var slide3 = '<div id="left-slide"><img id="logo-header" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/logo-header.png"><a><img id="droite" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/droite.png"></a><a><img id="gauche" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/gauche.png"></a><img id="slide" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/slide+3.jpg"><div id="nav"><a><img style="padding-left:3px;" id="slide_1" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-off.png"></a><img style="padding-left:3px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-link.png"><a><img style="padding-left:3px;" id="slide_2" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-off.png"></a><img style="padding-left:3px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-link.png"><a><img style="padding-left:3px;" id="slide_3" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/page-on.png"></a></div></div><div id="right-slide"><img id="pointe" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/Rectangle+5.png"><div id="right-container" class="row"><div class="columns medium-10 medium-offset-1 large-8 large-offset-1 end"><p id="anes">CHAQUE ANNÉE DES ÂNES SONT ABANDONNÉS, AFFAIBLIES...<br /><b id="hardonnerie">ILS VONT ÊTRE ACCUEILLIS À LA HARDONNERIE, NOTRE FERME REFUGE et ÉDUCATIVE</b></p><p id="agis">J\'agis pour les aider à passer l\'hiver</p><a href="#part-don"><img id="slide3-don" style="padding-top: 30px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/btn+don.png"></a></div></div><img id="dome" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/dome_blanc.png"></div>';
+var nbSlide = 1;
+
+$(document).on("click", "#slide_1", function() {
+	timer = 0;
+	$("#slider").html(slide1);
+	nbSlide = 1;
+});
+$(document).on("click", "#slide_2", function() {
+	timer = 0;
+	$("#slider").html(slide2);
+	nbSlide = 2;
+});
+$(document).on("click", "#slide_3", function() {
+	timer = 0;
+	$("#slider").html(slide3);
+	nbSlide = 3;
+});
+$(document).on("click", "#droite", function() {
+	timer = 0;
+	if (nbSlide === 1) {
+		$("#slider").html(slide2);
+		nbSlide = 2;
+	}
+	else if (nbSlide === 2) {
+		$("#slider").html(slide3);
+		nbSlide = 3;
+	}
+	else if (nbSlide === 3) {
+		$("#slider").html(slide1);
+		nbSlide = 1;
+	}
+});
+$(document).on("click", "#gauche", function() {
+	timer = 0;
+	if (nbSlide === 1) {
+		$("#slider").html(slide3);
+		nbSlide = 3;
+	}
+	else if (nbSlide === 2) {
+		$("#slider").html(slide1);
+		nbSlide = 1;
+	}
+	else if (nbSlide === 3) {
+		$("#slider").html(slide2);
+		nbSlide = 2;
+	}
+});
+
+function transition() {
+	if (timer >= 10) {
+		if (nbSlide === 1) {
+			$("#slider").html(slide2);
+			nbSlide = 2;
+		}
+		else if (nbSlide === 2) {
+			$("#slider").html(slide3);
+			nbSlide = 3;
+		}
+		else if (nbSlide === 3) {
+			$("#slider").html(slide1);
+			nbSlide = 1;
+		}
+		timer = 0;
+	}
+	else {
+		timer++;
+	}
+};
+var timer = 0;
+
+$('#sticky').click(function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
+$(document).on("click", "#slide1-don", function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
+
+$(document).on("click", "#slide2-don", function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
+
+$(document).on("click", "#slide3-don", function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
+
+
+
+
+
+var slide1Mob = '<div id="slider1Mobile"><img id="logo-header-mobile" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/logo+header+mobile.png"><div id="container-mobile" class="row"><div class="columns small-10 small-offset-1 text-center end"><p id="poules-mobile">ELLES NE VERRONT LA LUMIÈRE DU JOUR<br /><b id="abattoir-mobile">QUE POUR PARTIR À L\'ABATTOIR</b></p><p id="agis-mobile">J\'agis pour l\'accès des animaux au plein air</p><a href="#part-don"><img id="slide1-don-mobile" style="padding-top: 30px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/btn+don+mobile.png"></a></div></div><img id="dome-mobile" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/dome+blanc+mobile.png"></div></div>'
+var slide2Mob = '<div id="slider2Mobile"><img id="logo-header-mobile" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/logo+header+mobile.png"><div id="container-mobile" class="row"><div class="columns small-10 small-offset-1 text-center end"><p id="poules-mobile">FERME À SANG<br /><b id="abattoir-mobile">AVORTÉES SANS ANESTHÉSIE POUR PRODUIRE UNE HORMONE</b></p><p id="agis-mobile">J\'agis contre les maltraitances et mutilations</p><a href="#part-don"><img id="slide2-don-mobile" style="padding-top: 30px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/btn+don+mobile.png"></a></div></div><img id="dome-mobile" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/dome+blanc+mobile.png"></div></div>'
+var slide3Mob = '<div id="slider3Mobile"><img id="logo-header-mobile" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/logo+header+mobile.png"><div id="container-mobile" class="row"><div class="columns small-10 small-offset-1 text-center end"><p id="anes-mobile">ÂNES : ABANDONÉS, AFFAIBLIS...<br /><b id="hardonnerie-mobile">ILS ONT TROUVÉ REFUGE À LA HARDONNERIE, NOTRE FERME REFUGE ET ÉDUCATIVE</b></p><p id="agis-mobile">J\'agis pour les aider à passer l\'hiver</p><a href="#part-don"><img id="slide2-don-mobile" style="padding-top: 30px;" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/btn+don+mobile.png"></a></div></div><img id="dome-mobile" src="https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Noel-2017/mobile/dome+blanc+mobile.png"></div></div>'
+var nbSlideMob = 1;
+
+function transitionMob() {
+	console.log("timerMob =");
+	console.log(timerMob);
+	console.log("NbSlideMob =");
+	console.log(nbSlideMob);
+	if (timerMob >= 10) {
+		if (nbSlideMob === 1) {
+			$("#slider-mobile").html(slide2Mob);
+			nbSlideMob = 2;
+		}
+		else if (nbSlideMob === 2) {
+			$("#slider-mobile").html(slide3Mob);
+			nbSlideMob = 3;
+		}
+		else if (nbSlideMob === 3) {
+			$("#slider-mobile").html(slide1Mob);
+			nbSlideMob = 1;
+		}
+		timerMob = 0;
+	}
+	else {
+		timerMob++;
+	}
+};
+
+var timerMob = 0;
+
+$(document).ready(function() {
+	setInterval(transition, 1000);
+	setInterval(transitionMob, 1000);
+});
+
+$('#sticky-mobile').click(function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
+$(document).on("click", "#slide1-don-mobile", function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
+
+$(document).on("click", "#slide2-don-mobile", function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
+
+$(document).on("click", "#slide3-don-mobile", function(e) {
+	e.preventDefault();
+	$('html, body').animate({
+		scrollTop: $('#eq').offset().top}, 1000);
+});
