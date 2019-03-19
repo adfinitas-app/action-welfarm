4d87f5467025b7a9f7cc4a29c45f7f89126e42a4
Sébastien Bruere <sebastien.bruere@epitech.eu>
2019-03-19 17:18:37 -0400

    Add LP Poules pondeuses

---
+++ poules-pondeuses/assets/js/app.js
@@ -0,0 +1,135 @@
+function preload(arrayOfImages) {
+    $(arrayOfImages).each(function(){
+        $('<img/>')[0].src = this;
+    });
+}
+
+// Usage:
+
+preload([
+    "https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Poules-pondeuses/optin-unchecked.png",
+    "https://s3.amazonaws.com/heroku-adfinitas-campaign/Welfarm-Poules-pondeuses/optin-checked.png"
+]);
+
+
+var opts = {
+    angle: -0.5, // The span of the gauge arc
+    lineWidth: 0.04, // The line thickness
+    radiusScale: 1, // Relative radius
+    pointer: {
+        length: 0, // // Relative to gauge radius
+        strokeWidth: 0, // The thickness
+        color: '#959595' // Fill color
+    },
+    limitMax: false,     // If false, max value increases automatically if value > maxValue
+    limitMin: false,     // If true, the min value of the gauge will be fixed
+    colorStart: '#FFFFFF',   // Colors
+    colorStop: '#959595',    // just experiment with them
+    strokeColor: '#FFFFFF',  // to see which ones work best for you
+    generateGradient: true,
+    highDpiSupport: true,     // High resolution support
+};
+
+
+$(window).scroll( function () {
+    if ($(window).width() > 640)
+        HandleScrollForm();
+
+});
+
+$(document).ready( function () {
+    $("#f_phone").intlTelInput({
+        utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.13/js/utils.js",
+        initialCountry: "fr"
+    });
+
+    if ($(window).width() > 640)
+        HandleScrollForm();
+
+    $('#f_email').on('input', function() {
+        $('#form .hidden').slideDown();
+    });
+
+    let checkClick = false;
+
+
+    $('.petition p').click(function () {
+        checkClick = true;
+    });
+    $('#close').click( function (e) {
+        e.preventDefault();
+
+        $('#petition').focus();
+        $('body, html').css({margin: 0,height: 'auto',overflow: 'visible'});
+        $('.petition').hide();
+    });
+    $('#petition').click( function (e) {
+        e.preventDefault();
+
+        $('body, html').css({margin: 0,height: '100%',overflow: 'hidden'});
+        $('.petition p').focus();
+        $('.petition').show();
+    });
+
+    $('.petition').click( function () {
+        if ((!checkClick) && $('.petition').css('display') === 'block') {
+            $('body, html').css({margin: 0,height: 'auto',overflow: 'visible'});
+            $('.petition').hide();
+        }
+        checkClick = false;
+    });
+    $('#petition-bt-small').click(function (e) {
+        e.preventDefault();
+        $('#form').show();
+    });
+    $('#closeForm').click( function (e) {
+        e.preventDefault();
+        $('#form').hide();
+    })
+});
+
+
+function HandleScrollForm() {
+    if($('#form').offset().top + $('#form').height() >= $('#social').offset().top - 55)
+        $('#form').css({'position':'absolute', top: $('#form').offset().top});
+    if($(document).scrollTop() + window.innerHeight < $('#social').offset().top)
+        $('#form').css({'position':'fixed', top: 120});
+}
+
+
+function 	scrollToNext(next){
+    $('html, body').stop().animate({
+        scrollTop: $(next).offset().top - 0
+    }, 700, 'swing');
+}
+
+
+$.fn.gauge = function(opts) {
+    this.each(function() {
+        var $this = $(this),
+            data = $this.data();
+
+        if (data.gauge) {
+            data.gauge.stop();
+            delete data.gauge;
+        }
+        if (opts !== false) {
+            data.gauge = new Gauge(this).setOptions(opts);
+        }
+    });
+    return this;
+};
+
+
+function setGauge(gauge, nb) {
+    gauge.maxValue = 1000;
+    gauge.setMinValue(0);
+    gauge.animationSpeed = 32;
+    gauge.set(nb);
+
+    setTimeout(function(){
+        $('.gauge-container p').fadeIn();
+    }, 1500);
+}
+
+
---
+++ poules-pondeuses/assets/js/data.js
@@ -0,0 +1,149 @@
+
+function sendData() {
+    var data = {
+        "db": {
+            "schema": "welfarm_poules_2019",
+            "db": {
+                "email": pureField($('#f_email').val()),
+                "phone": pureField(getPhone()),
+                "firstname": pureField(getFirstname().toUpperCase()),
+                "lastname": pureField(getLastname().toUpperCase()),
+                "name":  pureField($('#f_name').val()),
+                "language": "fr_FR",
+            }
+        },
+        "mailjet": {
+            "Email": pureField($('#f_email').val()),
+            "Properties": {
+                "firstname": pureField(getFirstname().toUpperCase()),
+                "lastname": pureField(getLastname().toUpperCase()),
+                "name":  pureField($('#f_name').val()),
+                "language": "fr_FR"
+            },
+            "addLists": getList(), // Noms de transmission des listes dans lesquelles ajouter le contact. Ne pas mettre les listes "Toute la base" et "Prospects" ici, le contact y est inséré par défaut (excepté dans "Prospect" si donateur).
+            "delLists": []  // Noms de transmission des listes dans lesquelles supprimer le contact.
+        }
+    };
+    console.log(data);
+    //makeCorsRequest(data);
+}
+
+/*
+ * Debut de la lib
+ */
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length,c.length);
+    }
+    return "";
+}
+function createCORSRequest(method, url) {
+    var xhr = new XMLHttpRequest();
+    if ("withCredentials" in xhr) {
+        // XHR for Chrome/Firefox/Opera/Safari.
+        xhr.open(method, url, true);
+    } else if (typeof XDomainRequest != "undefined") {
+        // XDomainRequest for IE.
+        xhr = new XDomainRequest();
+        xhr.open(method, url);
+    } else {
+        // CORS not supported.
+        xhr = null;
+    }
+    return xhr;
+}
+function makeCorsRequest(data) {
+    var url = 'https://adfinitas-io.herokuapp.com/api/v1/organization/c2ff768a-aa71-4234-b082-3c5f67318cd7/webhook/a8d03cd4-f87e-4a3c-b898-bb0876da1269';
+    var body = JSON.stringify(data);
+    var xhr = createCORSRequest('POST', url);
+    if (!xhr) {
+        alert('CORS not supported');
+        return;
+    }
+    xhr.setRequestHeader('Content-Type', 'application/json');
+    xhr.send(body);
+}
+
+/*
+ * Fin de la lib
+ */
+function getUTM() {
+    var p = extractUrlParams();
+
+    if (p['utm_source'] && p['utm_source'] !== "undefined")
+        return p['utm_source'];
+    else
+        return "";
+}
+
+function getPhone() {
+    return $('#f_phone').intlTelInput("getNumber");
+}
+
+function getPersonnalisationCourte() {
+    return getCivilityLong().toUpperCase() + " " + pureField($('#f_lastname').val().toUpperCase());
+}
+
+function getPersonnalisation() {
+    return getCivilityDear() + " " + getCivilityLong().toUpperCase() + " " + pureField($('#f_lastname').val().toUpperCase());
+}
+
+function getList() {
+    var data = [];
+
+    data.push('petitionpoules2019');
+    return data;
+}
+
+function pureField(string) {
+    return (string.replace(/'/g, "%27").replace(/"/g, "&quot;"));
+}
+
+
+function getOptin() {
+    if ($('#f_optin').is(":checked")) {
+        return "false";
+    }
+    return "true";
+}
+
+function getSexe() {
+    if ($('#f_female').attr("checked") === "checked")
+        return "Femme";
+    else
+        return 'Homme';
+}
+function getFirstname() {
+    return $('#f_name').val().split(' ')[0];
+}
+function getLastname() {
+    if ($('#f_name').val().split(' ')[1] != null)
+        return $('#f_name').val().split(' ')[1];
+    else
+        return "";
+}
+function getCivility() {
+    if ($('#f_female').attr("checked") === "checked")
+        return "Mme";
+    else
+        return 'M';
+}
+
+function getCivilityDear() {
+    if ($('#f_female').attr("checked") === "checked")
+        return "Chère";
+    else
+        return 'Cher';
+}
+
+function getCivilityLong() {
+    if ($('#f_female').attr("checked") === "checked")
+        return "Madame";
+    else
+        return 'Monsieur';
+}
